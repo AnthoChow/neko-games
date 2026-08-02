e9ff196ff9ea67900db9f877975e7242e846c7a6
Anthony Chow <98489809+AnthoChow@users.noreply.github.com>
2026-08-02 01:42:00 -0700

    audio

--- js/player.js
+++ js/player.js
@@ -1,7 +1,7 @@
 const guestPuzzles = [
   { question: "/rgualmee ꖌᒷᒷ!¡╎リ⍊ᒷリℸ𝙹∷ǁ ____", answer: "/gamerule keepInventory true", bg: "bg-guest-1" },
   { question: "-.... .--- ---.. .-.. <span class='underline-hint'>.-.</span> <span class='underline-hint'>.-</span> -.", answer: "axolotl", bg: "bg-guest-2" },
-  { question: "Dandelion + Poppy + Orchid + Lily", answer: "2254", bg: "bg-guest-3" },
+  { question: "June 7 2022 Achievement", answer: "Sneak 100", bg: "bg-guest-3" },
   { question: "eTq3RtM5 - Poem Line #23 - Love", answer: "and the universe said I love you because you are love.", bg: "bg-guest-4" },
 ];
 
