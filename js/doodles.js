108d8b065bb500f2cf475c8a83bf370b96a4fcd5
Anthony Chow <98489809+AnthoChow@users.noreply.github.com>
2026-08-02 08:52:27 -0700

    x

--- js/doodles.js
+++ js/doodles.js
@@ -1,5 +1,5 @@
 const doodlesPuzzles = [
-  { question: "Doodles' clue #1 goes here...", answer: "answer1", bg: "bg-doodles" },
+  { question: "01001001 01010100", answer: "IT", bg: "bg-doodles" },
   { question: "Doodles' clue #2 goes here...", answer: "answer2", bg: "bg-doodles" },
   { question: "Doodles' clue #3 goes here...", answer: "answer3", bg: "bg-doodles" },
   { question: "Doodles' clue #4 goes here...", answer: "answer4", bg: "bg-doodles" },
