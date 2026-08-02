c896de1fe45049b8cb62a063ff0d2e5e0d54c0ba
Anthony Chow <98489809+AnthoChow@users.noreply.github.com>
2026-08-02 03:17:39 -0700

    castsfsfdsfds

--- js/meowdy.js
+++ js/meowdy.js
@@ -6,6 +6,50 @@ const meowdyPuzzles = [
   { question: "Meowdy's clue #5 goes here...", answer: "answer5", bg: "bg-meowdy" },
 ];
 
+function initHanoi(diskCount, container, onSolved) {
+  const pegs = [[], [], []];
+  for (let i = diskCount; i >= 1; i--) pegs[0].push(i);
+
+  let selectedPeg = null;
+  const pegEls = container.querySelectorAll(".peg");
+
+  function render() {
+    pegEls.forEach((pegEl, i) => {
+      pegEl.querySelectorAll(".disk").forEach(d => d.remove());
+      pegs[i].forEach(diskSize => {
+        const disk = document.createElement("div");
+        disk.className = "disk";
+        disk.style.width = `${diskSize * 15 + 20}px`;
+        pegEl.appendChild(disk);
+      });
+      pegEl.classList.remove("selected");
+    });
+    if (selectedPeg !== null) pegEls[selectedPeg].classList.add("selected");
+  }
+
+  function handleClick(index) {
+    if (selectedPeg === null) {
+      if (pegs[index].length === 0) return;
+      selectedPeg = index;
+    } else {
+      const from = pegs[selectedPeg];
+      const to = pegs[index];
+      const moving = from[from.length - 1];
+      const top = to[to.length - 1];
+
+      if (selectedPeg !== index && (to.length === 0 || moving < top)) {
+        to.push(from.pop());
+      }
+      selectedPeg = null;
+    }
+    render();
+    if (pegs[2].length === diskCount) onSolved();
+  }
+
+  pegEls.forEach((el, i) => el.addEventListener("click", () => handleClick(i)));
+  render();
+}
+
 function initHanoiMulti(puzzleData) {
   const container = document.getElementById("hanoiMultiContainer");
   container.style.display = "block";
@@ -39,7 +83,6 @@ function initHanoiMulti(puzzleData) {
         <div class="peg" data-peg="1"></div>
         <div class="peg" data-peg="2"></div>
       </div>
-      <p class="hanoi-moves">Moves: <span class="moveCount">0</span></p>
     `;
 
     initHanoi(puzzleData.diskCount || 3, boardWrap, () => {
