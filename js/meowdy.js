632bce213b0a781b37f2ecbb337808e13030f75a
Anthony Chow <98489809+AnthoChow@users.noreply.github.com>
2026-08-02 03:30:31 -0700

    2 daollasdsad

--- js/meowdy.js
+++ js/meowdy.js
@@ -1,5 +1,5 @@
 const meowdyPuzzles = [
-  { question: "Meowdy's clue #2 goes here...", answer: "Chiikawa", bg: "bg-meowdy" },
+  { question: "ツーダラー、ツーダラー", answer: "Chiikawa", bg: "bg-meowdy" },
   { question: "Meowdy's clue #2 goes here...", answer: "answer2", bg: "bg-meowdy" },
   { question: "Meowdy's clue #3 goes here...", answer: "answer3", bg: "bg-meowdy" },
   { question: "Meowdy's clue #4 goes here...", answer: "answer4", bg: "bg-meowdy" },
