4c399be8a261aa4bd371cd75488b851d299eaaa9
Anthony Chow <98489809+AnthoChow@users.noreply.github.com>
2026-08-02 21:00:53 -0700

    ss

--- js/meowdy.js
+++ js/meowdy.js
@@ -1,7 +1,7 @@
 const meowdyPuzzles = [
   { question: "ツーダラー、ツーダラー", answer: "Chiikawa", bg: "bg-meowdy" },
-  { question: "Meowdy's clue #2 goes here...", answer: "Popped Lung", bg: "bg-meowdy" },
+  { question: "In the final hour, a sharp breath, and my chest falls", answer: "Pneumothorax", bg: "bg-meowdy" },
   { question: "14 Foasb Gvck", answer: "Ramen Show", bg: "bg-meowdy" },
-  { question: "Meowdy's clue #4 goes here...", answer: "Kaworu", bg: "bg-meowdy" },
+  { question: "Adam - 5DVrEJx", answer: "Kaworu", bg: "bg-meowdy" },
 ];
 
