ccd603d11436c45530ec5a09cd83b17a0ce1c356
Anthony Chow <98489809+AnthoChow@users.noreply.github.com>
2026-08-04 05:25:39 -0700

    cds

--- js/doodles.js
+++ js/doodles.js
@@ -1,6 +1,6 @@
 const doodlesPuzzles = [
   { question: "01001001 01010100", answer: "IT", bg: "bg-doodles" },
   { question: "34 51 77 78 65 43 42 33", answer: "Silent Hill", bg: "bg-doodles" },
-  { question: "0: Number of characters clapping for Shinji: Number of Eva Episodes - Churros", answer: "Unit 01", bg: "bg-doodles" },
+  { question: "00: Number of characters clapping for Shinji: Number of Eva Episodes - Churros", answer: "Unit 01", bg: "bg-doodles" },
   { question: "False Name: Feet", answer: "2B", bg: "bg-doodles" },
 ];
--- js/recal.js
+++ js/recal.js
@@ -1,6 +1,11 @@
 const recalPuzzles = [
-  { question: "Recal's clue #1 goes here...", answer: "Valorant", bg: "bg-recal" },
-  { question: "Recal's clue #2 goes here...", answer: "Twitter", bg: "bg-recal" },
-  { question: "Recal's clue #3 goes here...", answer: "answer3", bg: "bg-recal" },
-  { question: "Recal's clue #4 goes here...", answer: "answer4", bg: "bg-recal" },
+  { question: "Нечто - Manezhnaya Square", answer: "Valorant", bg: "bg-recal" },
+  { 
+    type: "image", 
+    imageSrc: "assets/heartttt.png", 
+    answer: "Twitter", 
+    bg: "bg-recal" 
+  },
+  { question: "Four Leaf Clover - Integer of complex rarity, facing thyself and the first", answer: "67", bg: "bg-recal" },
+  { question: "Cat Tribe", answer: "Gooner", bg: "bg-recal" },
 ];
