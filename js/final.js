51722b152cf02e81c938c31e09f914c1a7cdd8c7
Anthony Chow <98489809+AnthoChow@users.noreply.github.com>
2026-08-07 04:21:12 -0700

    k

---
+++ js/final.js
@@ -0,0 +1,19 @@
+const message = `Congratulations! You made it to the very end of what I will admit is an annoyingly tiresome puzzle game.
+
+To whoever may read this, you are wonderful. The fact you've made it all the way here is a proof of your life. The chances you made it here weren't low, but the exact probable likelihood you decided to even arrive at this point is a 1 in infinite chance. There is no other you, no other version, no other idea of what could've happened.
+
+It does not matter how you got here, it just matters that you did. You lived, you tried, and you chose to be here. And for that, I am more than proud.
+
+I praise you for your achievements.
+I congratulate you for your success.
+I admire you for your efforts.
+I respect you for your determination.
+I applaud you for your progress.
+I appreciate you for your integrity.
+I cherish you for who you are.
+
+No matter how you are, who you are, how you feel, what you feel, know that you have done so well. For everything. I sincerely thank you for being here. For you are incredible.`;
+
+document.getElementById("finalText").textContent = message;
+
+const music = document.getElementById("finalMusic");
